2763c08ca88f1e3b5a557edd67859df88848329d
Cesar Santoyo <cesarsantoyo9@gmail.com>
2019-08-06 17:36:01 -0400

    Update cs3_main.m

--- casestudy3/cs3_main.m
+++ casestudy3/cs3_main.m
@@ -1,1 +1,12 @@
+%           Main file for generating plots for the first case study in the 
+%           CCTA paper. 
+%               
+% Paper:    A BarrierFunction Approach to Finite-Time Stochastic System Verification and Control
 %
+% Author:   Cesar G. Santoyo
+% Date:     August 6th, 2019
+% 
+% If you have any questions, e-mail the author at csantoyo@gatech.edu
+%
+% Description:  This script runs the results demonstrated in the CCTA
+%               paper for Caset Study 2.
